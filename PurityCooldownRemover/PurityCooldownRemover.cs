0394f3f65c6c2adf29f626c29f48f0b12ba0295c
Naomi Calabretta <me@arytonex.pw>
2021-04-07 19:14:43 +0200

    Initial commit

---
+++ PurityCooldownRemover/PurityCooldownRemover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace AryToNeX {
+
+	[BepInPlugin("pw.AryToNeX.PurityCooldownRemover", "Purity Cooldown Remover", "1.0.0")]
+
+    public class PurityCooldownRemover : BaseUnityPlugin {
+
+        public void Awake() {
+			On.RoR2.GenericSkill.CalculateFinalRechargeInterval += (original, self) => {
+				return Mathf.Min(self.baseRechargeInterval, Mathf.Max(0f, self.baseRechargeInterval * self.cooldownScale - self.flatCooldownReduction));
+			};
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Purity Cooldown Remover
+
+It is mainly a rewrite of [this other mod](https://thunderstore.io/package/ProfessorOwl/PurityCompletelyRemovesCooldowns/) BUT:
+ - I know what I am doing;
+ - This mod works properly.
+
+Plus, it works with RoR2's Anniversary Update, and it is [open source](https://github.com/AryToNeX/PurityCooldownRemover).
